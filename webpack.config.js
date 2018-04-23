c85dfbe5d10463109ff2954b5e84ebd666e26635
Peach <scdzwyxst@gmail.com>
2018-04-23 12:01:15 +0800

    Set Quill as a external dependency and a peer dependency

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
         libraryTarget: 'umd',
         library: 'MarkdownShortcuts'
     },
+    externals: {
+        quill: 'Quill',
+    },
     module: {
         loaders: [
             {
